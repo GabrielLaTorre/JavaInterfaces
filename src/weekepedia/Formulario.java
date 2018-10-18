ce513b881a237f15d8df85889107556e2b02ea90
Gabe <lt.gabriel96@gmail.com>
2018-10-17 22:18:27 -0300

    Interfaces en Java

---
+++ src/weekepedia/Formulario.java
@@ -0,0 +1,42 @@
+package weekepedia;
+
+import javax.swing.*;
+import javax.swing.event.*;
+import java.awt.event.*;
+
+public class Formulario extends JFrame implements ActionListener, ChangeListener{
+   
+    private JLabel label1;
+    private JRadioButton radio1;
+    private JButton boton1;
+    
+public Formulario(){
+    setLayout(null);
+    label1 = new JLabel("Aceptar términos y condiciones");
+    label1.setBounds(10,10,400,30);
+    add(label1);
+
+    radio1  = new JRadioButton("Acepto");
+    radio1.setBounds(10,50,100,30);
+    radio1.addChangeListener(this);
+    add(radio1);
+    
+    boton1 = new JButton("Aceptar");
+    boton1.setBounds(10,100,100,30);
+    add(boton1);
+    boton1.addActionListener(this);
+    boton1.setEnabled(false);
+ }
+
+public void stateChanged(ChangeEvent e){
+    if(radio1.isSelected() == true){
+        boton1.setEnabled(true);
+    } else {boton1.setEnabled(false);}
+ }
+
+public void actionPerformed(ActionEvent e){
+    if(e.getSource() == boton1){
+        System.exit(0);
+    }
+ }
+}
---
+++ src/weekepedia/Main.java
@@ -0,0 +1,14 @@
+package weekepedia;
+//Pequeño proyecto para practicar las interfaces JRadioButton y JButton, junto con ActionListener y ChangeListener en Java
+public class Main {
+
+    public static void main(String[] args) {
+
+        Formulario formulario1 = new Formulario();
+        formulario1.setBounds(0, 0, 350, 200);
+        formulario1.setVisible(true);
+        formulario1.setResizable(false);
+        formulario1.setLocationRelativeTo(null);
+    }
+    
+}
